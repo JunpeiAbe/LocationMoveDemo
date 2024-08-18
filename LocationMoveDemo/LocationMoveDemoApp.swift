35f4991aeb7c0a73dd3786598b558662acca1d98
JunpeiAbe <uihrsw0228@gmail.com>
2024-08-18 19:13:09 +0900

    Initial Commit

---
+++ LocationMoveDemo/LocationMoveDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationMoveDemoApp.swift
+//  LocationMoveDemo
+//
+//  Created by j on 2024/08/18.
+//
+
+import SwiftUI
+
+@main
+struct LocationMoveDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
