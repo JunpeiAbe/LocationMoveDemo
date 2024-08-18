1b2d2ab20a2e408baf0a2cda81d2f76f7e59ee45
JunpeiAbe <uihrsw0228@gmail.com>
2024-08-18 19:34:19 +0900

    初期設定

--- LocationMoveDemo/ContentView.swift
+++ LocationMoveDemo/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  LocationMoveDemo
-//
-//  Created by j on 2024/08/18.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- LocationMoveDemo/LocationMoveDemoApp.swift
+++ LocationMoveDemo/LocationMoveDemoApp.swift
@@ -1,10 +1,3 @@
-//
-//  LocationMoveDemoApp.swift
-//  LocationMoveDemo
-//
-//  Created by j on 2024/08/18.
-//
-
 import SwiftUI
 
 @main
